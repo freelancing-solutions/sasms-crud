cebce0afca66ca688238642eb2209a334d36d151
justice <mobiusndou@gmail.com>
2019-10-09 20:08:15 +0200

    commit

--- database/Schema.js
+++ database/Schema.js
@@ -1,7 +1,7 @@
 let config = require('config');
 let mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || config.get("MONGODB_URI"), {useNewUrlParser : true});
+mongoose.connect(process.env.MONGODB_URI || config.get("MONGODB_URI"), {useUnifiedTopology : true});
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
 db.once("opne", () => console.log('Conntected to mongoose'));
