c8db93bf1490482a2101132b34bfc664bd6cbefa
justice <mobiusndou@gmail.com>
2019-10-09 20:06:57 +0200

    commit

--- database/Schema.js
+++ database/Schema.js
@@ -1,4 +1,10 @@
+let config = require('config');
 let mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || config.get("MONGODB_URI"), {useNewUrlParser : true});
+const db = mongoose.connection;
+db.on('error', error => console.log(error));
+db.once("opne", () => console.log('Conntected to mongoose'));
 
 exports.User = mongoose.model('User', require('./UserSchema'));
 exports.Accounts = mongoose.model('Accounts', require('./AccountsSchema'));
