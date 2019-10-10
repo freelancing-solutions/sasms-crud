21144cb3226a0598db3693844e69210e905d38a0
justice <mobiusndou@gmail.com>
2019-10-10 09:19:10 +0200

    commit

--- database/Schema.js
+++ database/Schema.js
@@ -2,6 +2,8 @@
 let config = require('config');
 let mongoose = require('mongoose');
 
+
+// try using mongodb atlas to add mongo
 mongoose.connect(process.env.MONGODB_URI || config.get("MONGODB_URI"), {useNewUrlParser : true, useUnifiedTopology : true});
 const db = mongoose.connection;
 db.on('error', error => console.log(error));
